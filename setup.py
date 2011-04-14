541dce0c6c1864d382d37153ca1231d5ef62cecd
icemac <icemac@62d5b8a3-27da-0310-9561-8e5933582275>
2011-04-14 06:51:38 +0000

    - Added test extra to declare test dependency on ``zope.testing``. - Added change log to tell that the packackes seems not to be compatible with Python 2.6+
    git-svn-id: file:///Users/mac/vcs/main/zc.set/trunk@121426 62d5b8a3-27da-0310-9561-8e5933582275

--- setup.py
+++ setup.py
@@ -1,9 +1,19 @@
 from setuptools import setup, find_packages
+import os.path
+
+def read(path):
+    return open(
+        os.path.join(os.path.dirname(__file__), *path.split('/'))).read()
+
 
 setup(
     name="zc.set",
     version="0.1dev",
     license='ZPL 2.1',
+    long_description='\n\n'.join([
+        read('CHANGES.txt'),
+        read('src/zc/set/README.txt'),
+        ]),
     packages=find_packages('src'),
     package_dir={'':'src'},
     namespace_packages=['zc'],
@@ -13,5 +23,9 @@ setup(
         'ZODB3',
         'zope.app.folder',
     ],
+    extras_require=dict(
+        test=[
+            'zope.testing',
+            ]),
     zip_safe=False
     )
